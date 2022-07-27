285f8cee43edfa40f8fa891a1476e29e2fd93d92
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2022-07-27 05:52:59 -0300

    chore: docker-compose

---
+++ src/main/java/br/com/vcmidia/keycript/KeycriptApplication.java
@@ -0,0 +1,13 @@
+package br.com.vcmidia.keycript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/vcmidia/keycript/KeycriptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vcmidia.keycript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
