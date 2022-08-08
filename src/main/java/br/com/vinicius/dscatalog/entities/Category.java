28e3fa38928c06b106b5446ecf9bcca98403acaf
viniciuscavalheiro <vinicius.cavalheiro@agrotis.com>
2022-08-08 05:54:34 -0300

    chore: entitie category

---
+++ src/main/java/br/com/vinicius/dscatalog/entities/Category.java
@@ -0,0 +1,55 @@
+package br.com.vinicius.dscatalog.entities;
+
+import java.util.Objects;
+
+public class Category {
+
+    private Long id;
+    private String name;
+
+    public Category() {
+
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
